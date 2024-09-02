6c18f1f0534d52dadc931b2e4901917ba4beb955
ozantalay <ozntalay@hotmail.com>
2024-09-02 19:27:28 +0300

    baslangic

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useContext, useEffect, useState } from "react";
+
+function App() {
+  const [userState, setUserState] = useState({
+    Namık: true,
+    Eda: true,
+    Suzan: true,
+    Engin: true,
+    Samet: true
+  });
+
+  // KODUNUZ BURAYA GELECEK
+}
+
+const UserList = () => {
+  // KODUNUZ BURAYA GELECEK
+};
+
+export default App;
